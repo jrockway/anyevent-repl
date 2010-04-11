f4d6600f545021b47be0227b7be0b740931ca603
Jonathan Rockway <jon@jrock.us>
2010-04-11 10:35:33 -0500

    add a terminal-based REPL

---
+++ lib/AnyEvent/REPL/Terminal.pm
@@ -0,0 +1,72 @@
+use MooseX::Declare;
+
+# example.
+
+class AnyEvent::REPL::Terminal with MooseX::Runnable {
+    use AnyEvent::REPL;
+    use Term::ReadLine;
+    use AnyEvent::Term;
+    use AnyEvent::Term::ReadLine;
+    use AnyEvent::Pump qw(pump);
+
+    has 'repl' => (
+        is      => 'ro',
+        isa     => 'AnyEvent::REPL',
+        default => sub { AnyEvent::REPL->new },
+    );
+
+    has 'input' => (
+        is      => 'ro',
+        isa     => 'AnyEvent::Term::ReadLine',
+        default => sub {
+            my $self = shift;
+            AnyEvent::Term::ReadLine->new(
+                prompt   => 'PERL> ',
+                on_error => sub { warn "Dying from terminal illness"; exit 1 },
+            );
+        },
+    );
+
+    method run {
+        my $term = AnyEvent::Term->instance;
+        $term->stdin_handle->on_read(sub {
+            my $h = shift;
+            if( $h->{rbuf} =~ /^(.*)$/ ){
+                $self->repl->kill(9);
+                $h->{rbuf} = $1;
+            }
+        });
+        my $done = 0;
+        while( !$done ){
+            my $prompt_pump = pump $self->input->pty->handle, $term;
+            my $input_pump  = pump $term, $self->input->pty->handle;
+            my $cv = AnyEvent->condvar;
+
+            $self->input->push_readline( sub {
+                my $line = shift;
+                $term->push_write("\n");
+
+                undef $prompt_pump;
+                undef $input_pump;
+
+                if($line =~ /^,exit$/){
+                    $done = 1;
+                    $cv->send('');
+                }
+
+                $self->repl->push_eval(
+                    $line,
+                    on_output => sub { $term->push_write(@_) },
+                    on_error  => $cv,
+                    on_result => $cv,
+                );
+            });
+
+            $term->push_write($cv->recv);
+        };
+
+        $term->DEMOLISH; # safer than SMASH_WITH_HAMMER
+
+        return 0;
+    }
+}
